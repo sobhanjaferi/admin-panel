e295e636dab63d4069a20a5c66784f0a232fecec
sobhan <sobhanjafarii87@gmail.com>
2026-08-10 12:59:06 +0330

    [ADD] aside panel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Aside from "@/features/Aside/Aside";
+
 const Home = () => {
   return (
-    <section>
-      <h1>hello world</h1>
+    <section className="grid grid-cols-5">
+      <section className="col-span-1">
+        <Aside />
+      </section>
+
+      <section className="col-span-4 p-5">
+        <h1>hello world</h1>
+      </section>
     </section>
   );
 };
