514d37e32a7b2a596dff123e2866298bcedc42d2
sobhan <sobhanjafarii87@gmail.com>
2026-08-11 20:42:12 +0330

    [EDIT] layout styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col bg-gray-100 text-gray-600">{children}</body>
     </html>
   );
 }
--- src/features/Aside/ListItem.tsx
+++ src/features/Aside/ListItem.tsx
@@ -14,10 +14,13 @@ const Li = ({ icon, title, link, className, ...otherProps }: Props) => {
   const url: string = usePathname();
   return (
     <li
-      className={`cursor-pointer w-full p-3 rounded-md hover:bg-cyan-600 ${url === link && "bg-cyan-600"} active:opacity-30 ${className}`}
+      className={`cursor-pointer w-full p-3 rounded-md hover:bg-cyan-600 active:opacity-30 ${url === link && "bg-cyan-600"} ${className}`}
       {...otherProps}
     >
-      <Link href={link} className="flex gap-2 justify-start items-center">
+      <Link
+        href={link}
+        className="flex gap-2 justify-start items-center outline-0"
+      >
         {icon} <span>{title}</span>
       </Link>
     </li>
