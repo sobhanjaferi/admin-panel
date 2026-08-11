285b0eaa02e6917bd012767815c93e79869f2eee
sobhan <sobhanjafarii87@gmail.com>
2026-08-11 20:21:51 +0330

    [UPDATE] list item styles

--- src/features/Aside/ListItem.tsx
+++ src/features/Aside/ListItem.tsx
@@ -1,18 +1,23 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { ComponentProps, ReactNode } from "react";
 
 type Props = ComponentProps<"li"> & {
   icon: ReactNode;
   title: string;
+  link: string;
 };
 
-const Li = ({ icon, title, className, ...otherProps }: Props) => {
+const Li = ({ icon, title, link, className, ...otherProps }: Props) => {
+  const url: string = usePathname();
   return (
     <li
-      className={`cursor-pointer w-full p-3 rounded-md hover:bg-cyan-600 active:opacity-30 ${className}`}
+      className={`cursor-pointer w-full p-3 rounded-md hover:bg-cyan-600 ${url === link && "bg-cyan-600"} active:opacity-30 ${className}`}
       {...otherProps}
     >
-      <Link href={""} className="flex gap-2 justify-start items-center">
+      <Link href={link} className="flex gap-2 justify-start items-center">
         {icon} <span>{title}</span>
       </Link>
     </li>
